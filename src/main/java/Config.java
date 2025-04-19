cbc9cded13325f929a9d1c39cd8667ec506f2505
diygun <diygunfr@gmail.com>
2025-04-19 13:31:50 +0200

    JWT update

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,5 @@
 public class Config {
     public static final String BASE_URL = "http://145.239.196.116";
-    public static final String JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ODU2NjA2OTI3IiwiZW1haWwiOiJqb3N1ZS5jYXN0ZWxsYW5pQHN0dWRlbnQuaGVwbC5iZSIsImxvZ2luIjoiam9zdWVlIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzQ1MDYwNTU4LCJleHAiOjE3NjA2MTI1NTh9.-t96EnJhZzox8KJinoiOdrHv48nHOapnFGha3V61rhY";
+    public static final String JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMjk1OTE4NDI5IiwiZW1haWwiOiJzaW1nZW9yZ2VzMDAwQGdtYWlsLmNvbSIsImxvZ2luIjoic2ltb25hZG1pbmlzdHJhdG9yIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzQ0NjMxODAzfQ.UuzDDzRk9YB8Puu9ltfr6ZixurJdqRjaQ1SGz9P-wlY";
 
 }
