6e762d55140a6f6d0e6492576565b5abdb8fdbf0
diygun <diygunfr@gmail.com>
2025-04-24 00:32:41 +0200

    Update link to https

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,5 @@
 public class Config {
-    public static final String BASE_URL = "http://145.239.196.116";
+    public static final String BASE_URL = "https://3dsecure.makeitnextgen.com";
     public static final String JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMjk1OTE4NDI5IiwiZW1haWwiOiJzaW1nZW9yZ2VzMDAwQGdtYWlsLmNvbSIsImxvZ2luIjoic2ltb25hZG1pbmlzdHJhdG9yIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzQ0OTQxODAzfQ.7BkFeUbO8uSl8OwRG9bOKoDRf1kZHBclFNXdZ4Rkuhc";
 
 }
